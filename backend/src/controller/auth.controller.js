7da945bec45ea7a822d571f1c3127eb2d6d900da
LuvBhatia <luvbhatia20005@gmail.com>
2025-12-25 01:39:49 +0530

    completing signup

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,7 +1,52 @@
-export function signup(req, res) {
-    res.send("Signup Route");
-}
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"; 
 
+export async function signup(req, res) {
+    const { email, password, fullName } = req.body;
+
+
+    try{
+        if (!email || !password || !fullName) {
+            return res.status(400).json({ message: "All fields are required." });
+        }
+
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Password must be at least 8 characters long." });
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email is already registered." });
+        }
+        const idx=Math.floor(Math.random()*1000)+1;
+        const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`;
+
+        const newUser = await User.create({
+            email,
+            password,
+            fullName,
+            profilePic:randomAvatar
+        })
+        const token =jwt.sign({userId:newUser._id},process.env.JWT_SECRET,{expiresIn:'7d'})
+        res.cookie("jwt",token,{
+            maxAge:7*24*60*60*1000,
+            httpOnly:true,
+            sameSite:"strict",
+            secure:process.env.NODE_ENV==="production"
+        })
+        res.status(201).json({sucess:true, user:newUser})
+       
+        
+
+    }catch(error){
+        console.log("Error during signup:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+        
 export function login(req, res) {
     res.send("Login Route");
 }
